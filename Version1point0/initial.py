528d9faf51c44d02bd389acc8d00642b1bdf8094
Admin <admin@Admins-MacBook-Pro.local>
2020-02-15 18:56:15 -0500

    getting set up

--- Version1point0/initial.py
+++ Version1point0/initial.py
@@ -1,15 +1,29 @@
 # 02-12-2020
 # following along to a video showing how to implement simple neural network
+def main():
 
 import numpy as np
 
-def sigmoid(x):
+__main__
+
+    def sigmoid(x):
     return 1 / (1  + np.exp(-x))
 
- # training input data
-training_input_data = np.array([0,0,1],
+    # training input data
+    training_input_data = np.array([0,1,0],
                                [1,1,1],
                                [1,0,1],
-                               [0,1,1]])
+                               [0,1,0]])
 
-training_output_data = np.array([[0,1,1,0]]).T
+    training_output_data = np.array([[0,1,1,0]]).T
+
+    print('Random starting synaptic weights: ')
+    print(synaptic_weights)
+
+    for iteration in range(1)
+
+        input_layer = training_inputs
+
+        outputs = sigmoid(np.dot(training))
+
+if __name__ == "__main__": main()
