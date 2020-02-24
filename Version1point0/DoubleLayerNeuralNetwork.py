53a7e8c1a6649d3e1cf3607ea5040a0582357e54
Admin <admin@Admins-MacBook-Pro.local>
2020-02-24 17:11:22 -0500

    starting double layer neural network

---
+++ Version1point0/DoubleLayerNeuralNetwork.py
@@ -0,0 +1,56 @@
+class Neural Network():
+
+    def __init__(self):
+        np.random.seed(1)
+
+        self.synaptic.weight
+
+        import numpy as np
+
+        # sigmoid function is used to normalize, negatives values return a value below 1/2 and approaching zero, positive values return above 1/2 and approaching 1
+        def sigmoid(x):
+            return 1 / (1 + np.exp(-x))
+
+        #
+        def sigmoid_derivative(x):
+            return x * (1 - x)
+
+        # training input data
+
+        training_input_data = np.array([
+            [0, 1, 0],
+            [1, 1, 1],
+            [1, 0, 1],
+            [0, 1, 0]])
+
+        training_output_data = np.array([[0, 1, 1, 0]]).T
+
+        synaptic_weights = 2 * np.random.random((3, 1)) - 1
+
+        print('Initial Random starting synaptic weights: ')
+        print(synaptic_weights)
+
+        input_layer = training_input_data
+
+        for iteration in range(20):
+            outputs = sigmoid(np.dot(training_input_data, synaptic_weights))
+
+            error = training_output_data - outputs
+
+            adjustments = error + sigmoid_derivative(outputs)
+
+            synaptic_weights += np.dot(input_layer.T, adjustments)
+
+            print('Synaptic weights after training')
+            print(synaptic_weights)
+
+        print('Synaptic weights after training')
+        print(synaptic_weights)
+
+        print('Outputs after training')
+        print(outputs)
+
+
+
+
+
--- Version1point0/initial.py
+++ Version1point0/initial.py
@@ -1,16 +1,20 @@
 # 02-12-2020
 # following along to a video showing how to implement simple neural network
 
+# this is a perceptron, meaning it has only one layer
+
 import numpy as np
 
+# sigmoid function is used to normalize, negatives values return a value below 1/2 and approaching zero, positive values return above 1/2 and approaching 1
 def sigmoid(x):
 
     return 1 / (1 + np.exp(-x))
-
+#
 def sigmoid_derivative(x):
 
     return x * (1 - x)
-    # training input data
+
+# training input data
 
 training_input_data = np.array([
     [0,1,0],
@@ -25,9 +29,9 @@ synaptic_weights = 2 * np.random.random((3,1)) - 1
 print('Initial Random starting synaptic weights: ')
 print(synaptic_weights)
 
-for iteration in range(500000):
+input_layer = training_input_data
 
-    input_layer = training_input_data
+for iteration in range(20):
 
     outputs = sigmoid(np.dot(training_input_data,synaptic_weights))
 
@@ -36,6 +40,9 @@ for iteration in range(500000):
     adjustments = error + sigmoid_derivative(outputs)
 
     synaptic_weights += np.dot(input_layer.T, adjustments)
+
+    print('Synaptic weights after training')
+    print(synaptic_weights)
 
 print('Synaptic weights after training')
 print(synaptic_weights)
